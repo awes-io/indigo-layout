878695cc8c1c0e652dff6c785af650db0de18764
venegrad <venegrad@gmail.com>
2019-01-16 13:37:00 +0200

    AWS-341-2

--- dist/js/main.js
+++ dist/js/main.js
@@ -665,7 +665,7 @@
                     document.querySelectorAll('pre code.html').forEach( block => { hljs.highlightBlock(block); });
                 }
             },
-            'highlight_css': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/atom-one-dark.min.css'
+            'highlight_css': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/github-gist.min.css'
         },
 
         install() {
