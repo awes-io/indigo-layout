4314479251b818afeda305650bf62b84fdd1ac0e
theAlex <info@thealex.ru>
2019-01-27 14:14:07 +0300

    Updated serviceProvider

--- src/IndigoLayoutServiceProvider.php
+++ src/IndigoLayoutServiceProvider.php
@@ -2,42 +2,18 @@
 
 namespace AwesIO\IndigoLayout;
 
-use Illuminate\Support\ServiceProvider;
-use AwesIO\BaseJS\PackageTools;
+use AwesIO\BaseJS\AwesProvider;
 
-class IndigoLayoutServiceProvider extends ServiceProvider
+class IndigoLayoutServiceProvider extends AwesProvider
 {
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function getPackageName(): string
     {
-        $this->publishes([
-            __DIR__.'/../config/indigo-layout.php' => config_path('indigo-layout.php'),
-        ], 'config');
-
-        $this->publishes([
-            __DIR__.'/../dist' => public_path('static/indigo-layout'),
-        ], 'awes-public');
-
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'indigo-layout');
-
-        PackageTools::addSrc('indigo-layout', config('indigo-layout.src'));
+        return 'indigo-layout';
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function getPath(): string
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/indigo-layout.php',
-            'indigo-layout'
-        );
+       return __DIR__;
     }
 }
