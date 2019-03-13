88d02a410aa827b53df9a0c5deff40e99f48199e
Yevhen L <yevhen.lisovenko@awescode.de>
2019-03-13 17:29:27 +0000

    Update head.blade.php

--- resources/views/chunks/head.blade.php
+++ resources/views/chunks/head.blade.php
@@ -1,7 +1,7 @@
 <head>
+    <meta charset="utf-8">
     <title>@yield('meta_title', '')</title>
     <meta name="description" content="@yield('meta_description', '')">
-    <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700&amp;subset=cyrillic" rel="stylesheet">
