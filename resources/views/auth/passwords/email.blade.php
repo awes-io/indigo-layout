3b52ca0e58cec6ec8374085ab0e1f90adc0095ae
Yevhen L <yevhen.lisovenko@awescode.de>
2019-03-14 07:39:30 +0000

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,3 +1,3 @@
-<form-builder disabled-dialog="" url="route('password.email')" send-text="Reset Password">
+<form-builder disabled-dialog="" url="{{ route('password.email') }}" send-text="Reset Password">
     <fb-input name="email" type="email" label="E-Mail" focus></fb-input>
 </form-builder>
