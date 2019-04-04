5e8b8bf4ebd393362fa483bdfb4977b98abcabb3
venegrad <venegrad@gmail.com>
2019-04-04 16:05:23 +0300

    #8: add supports charts for card

--- resources/views/components/card/chart.blade.php
+++ resources/views/components/card/chart.blade.php
@@ -46,81 +46,75 @@
 {{-- Render the component --}}
 @if ($render)
 <content-wrapper
-    class="chart-wrap"
+    class="card card_chart"
     store-data="{{ $query }}"
     @if (isset($default_data) && !empty($default_data))
         :default='@json($default_data)'
     @endif
     :url="`{{ $api_url }}?{{ $filter_default_name }}=${$get($route, 'query.{{ $query }}', {{ $filter_default_value }})}`">
     <template slot-scope="chartData">
-        <div class="chart-box">
-            @if (isset($filter))
-                <div class="btn-group">
-                    @foreach($filter as $_days => $_title)
-                        <filter-builder label="{{ $_title }}" :param="{!! "{'$query': $_days}" !!}"></filter-builder>
-                    @endforeach
-                </div>
-            @endif
+        @if (isset($filter))
+            <div class="btn-group">
+                @foreach($filter as $_days => $_title)
+                    <filter-builder label="{{ $_title }}" :param="{!! "{'$query': $_days}" !!}"></filter-builder>
+                @endforeach
+            </div>
+        @endif        <chart-builder                class="card__chart"
+                :data='chartData'
+                :options="{elements: {line: {tension: 0,backgroundColor: '{{ $color }}',borderColor: '{{ $color }}'},point: {radius: 0}},legend: {display: false},layout: {padding: {top: 20}},scales: {xAxes: [{display: false}],yAxes: [{display: false}]},maintainAspectRatio: false}">
+        </chart-builder>
 
-            <chart-builder
-                    class="chart-box__chart"
-                    :data='chartData'
-                    :options="{elements: {line: {tension: 0,backgroundColor: '{{ $color }}',borderColor: '{{ $color }}'},point: {radius: 0}},legend: {display: false},layout: {padding: {top: 20}},scales: {xAxes: [{display: false}],yAxes: [{display: false}]},maintainAspectRatio: false}">
-            </chart-builder>
+        <div class="card__chart" style="background: {{ $color }};"></div>
 
-            <div class="chart-box__chart" style="background: {{ $color }};"></div>
-
-            <div class="chart-box__wrap">
-                <div class="chart-box__info">
-                    <span class="block tf-size-small tf-light tf-upper mb-md">{{ (isset($label)) ? $label : "" }}</span>
-                    <span class="block tf-size-small tf-light tf-upper mb-sm">{{ (isset($title)) ? $title : "" }}</span>
-                    <span class="block tf-size-title tf-light tf-upper">{{ (isset($value)) ? $value : "" }}</span>
-                    @if (isset($read_more))
-                        <div class="mt-sl">
-                            <a href="{{ isset($read_more['url']) ? $read_more['url'] : "#" }}" class="tf-hyplink">{{ isset($read_more['name']) ? $read_more['name'] : __('indigo-layout::common.read_more') }}</a>
-                        </div>
-                    @endif
-                </div>
+        <div class="card__wrap">
+            <div class="card__info">
+                <span class="block tf-size-small tf-light tf-upper mb-md">{{ (isset($label)) ? $label : "" }}</span>
+                <span class="block tf-size-small tf-light tf-upper mb-sm">{{ (isset($title)) ? $title : "" }}</span>
+                <span class="block tf-size-title tf-light tf-upper">{{ (isset($value)) ? $value : "" }}</span>
+                @if (isset($read_more))
+                    <div class="mt-sl">
+                        <a href="{{ isset($read_more['url']) ? $read_more['url'] : "#" }}" class="tf-hyplink">{{ isset($read_more['name']) ? $read_more['name'] : __('indigo-layout::common.read_more') }}</a>
+                    </div>
+                @endif
             </div>
         </div>
     </template>
     <template slot="loading">
-        <div class="chart-box chart-box_nodata loading-block" data-loading="{{ __('indigo-layout::common.loading') }}">
-            <div class="chart-box__wrap" >
-                <i class="icon icon-graph chart-box__icon"></i>
-            </div>
+        <div class="card__wrap card_nodata loading-block" data-loading="{{ __('indigo-layout::common.loading') }}">
+            <i class="icon icon-graph chart-box__icon"></i>
         </div>
     </template>
     <template slot="error">
-        <div class="chart-box chart-box_error">
-            <div class="chart-box__wrap">
-                <i class="icon icon-data-error chart-box__icon"></i>
-                <div class="chart-box__info">
-                    <span class="chart-box__info-redcapt">{{ __('indigo-layout::common.chart.error.parameters') }}</span>
+        <div class="card__wrap card_error">
+            <div class="card__middle-cell">
+                <i class="icon icon-data-error card__icon cl-red"></i>
+                <div class="card__info">
+                    <span class="card__info-redcapt cl-red">{{ __('indigo-layout::common.chart.error.loading') }}</span>
                 </div>
             </div>
         </div>
     </template>
     <template slot="empty">
-        <div class="chart-box chart-box_nodata">
-            <div class="chart-box__wrap">
-                <i class="icon icon-data-time chart-box__icon"></i>
-                <div class="chart-box__info">
-                    <span class="chart-box__info-label">{{ __('indigo-layout::common.chart.no-data') }}</span>
+        <div class="card__wrap card_nodata">
+            <div class="card__middle-cell">
+                <i class="icon icon-database-error card__icon cl-caption"></i>
+                <div class="card__info">
+                    <span class="card__info-label">{{ __('indigo-layout::common.chart.no-data') }}</span>
                 </div>
             </div>
         </div>
     </template>
 </content-wrapper>
 @else
-    <div class="chart-box chart-box_error">
-        <div class="chart-box__wrap">
-            <i class="icon icon-data-error chart-box__icon"></i>
-            <div class="chart-box__info">
-                <span class="chart-box__info-redcapt">{{ __('indigo-layout::common.chart.error.parameters') }}</span>
+    <div class="card card_chart">
+        <div class="card__wrap card_error">
+            <div class="card__middle-cell">
+                <i class="icon icon-data-error card__icon cl-red"></i>
+                <div class="card__info">
+                    <span class="card__caption cl-red">{{ __('indigo-layout::common.chart.error.parameters') }}</span>
+                </div>
             </div>
         </div>
     </div>
-
 @endif
 
