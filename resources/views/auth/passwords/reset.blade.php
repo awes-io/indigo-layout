758ccca99859c3ae22445c6de0580ad6b8835bab
Alexander <falkonseo@gmail.com>
2019-06-25 22:51:12 +0300

    Added password autocomplete attribute

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,6 +1,6 @@
 <form-builder disabled-dialog="" url="{{ route('password.update') }}" send-text="Change Password">
 	<fb-input name="email" label="{{ __('indigo-layout::auth.general.email') }}" autofocus required autocomplete="email"></fb-input>
 	<fb-input type="hidden" name="token" value="{{ $token }}"></fb-input>
-    <fb-input name="password" label="{{ __('indigo-layout::auth.general.password') }}" type="password"></fb-input>
-    <fb-input name="password_confirmation" label="{{ __('indigo-layout::auth.general.password_confirmation') }}" type="password"></fb-input>
+    <fb-input name="password" label="{{ __('indigo-layout::auth.general.password') }}" type="password" required autocomplete="new-password"></fb-input>
+    <fb-input name="password_confirmation" label="{{ __('indigo-layout::auth.general.password_confirmation') }}" type="password" required autocomplete="new-password"></fb-input>
 </form-builder>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,6 @@
     <fb-input name="email" type="email" label="{{ __('indigo-layout::auth.general.email') }}" autofocus required autocomplete="email"></fb-input>
     <fb-input name="name" label="{{ __('indigo-layout::auth.register.name') }}" autocomplete="name"></fb-input>
     <fb-input name="company" label="{{ __('indigo-layout::auth.register.company') }}"></fb-input>
-    <fb-input name="password" label="{{ __('indigo-layout::auth.general.password') }}" type="password"></fb-input>
-    <fb-input name="password_confirmation" label="{{ __('indigo-layout::auth.general.password_confirmation') }}" type="password"></fb-input>
+    <fb-input name="password" label="{{ __('indigo-layout::auth.general.password') }}" type="password" required autocomplete="new-password"></fb-input>
+    <fb-input name="password_confirmation" label="{{ __('indigo-layout::auth.general.password_confirmation') }}" type="password" required autocomplete="new-password"></fb-input>
 </form-builder>
