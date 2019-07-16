13015f04695bca4739b4a97708cdb36182b280b6
begimov <begimov@gmail.com>
2019-07-16 14:10:48 +0300

    localization

---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'general' => [
+        'email' => 'E-Mail',
+        'password' => 'Пароль',
+        'password_confirmation' => 'Подтверждение пароля'
+    ],
+
+    'login' => [
+        'remember' => 'Запомнить',
+        'forgot_link' => 'Забыли пароль?',
+        'send_text' => 'Войти'
+    ],
+    'register' => [
+        'name' => 'ФИО',
+        'company' => 'Компания (Необязательно)',
+        'send_text' => 'Зарегистрироваться'
+    ],
+    'passwords' => [
+        'email' => [
+            'send_text' => 'Сбросить пароль'
+        ],
+        'reset' => [
+            'send_text' => 'Изменить пароль'
+        ]
+    ]
+];
